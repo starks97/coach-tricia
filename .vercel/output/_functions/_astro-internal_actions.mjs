795281d0d991a8208f925557efaab69adf7fd2e1
Jefferson <ifrit68@hotmail.com>
2025-04-16 18:54:00 -0400

    new improves

---
+++ .vercel/output/_functions/_astro-internal_actions.mjs
@@ -0,0 +1,340 @@
+import './chunks/astro/server_DgSEdpN0.mjs';
+import { g as getActionQueryString, a as astroCalledServerError, A as ActionError, d as deserializeActionResult, b as ACTION_QUERY_PARAMS, c as appendForwardSlash } from './chunks/astro-designed-error-pages_C-BNchek.mjs';
+import { d as defineAction, o as objectType, s as stringType } from './chunks/server_rmDcfU8i.mjs';
+import { createTransport } from 'nodemailer';
+import { c as createInvalidVariablesError, g as getEnv$1, s as setOnSetGetEnv } from './chunks/runtime_DZGcX6x2.mjs';
+
+const apiContextRoutesSymbol = Symbol.for("context.routes");
+const ENCODED_DOT = "%2E";
+function toActionProxy(actionCallback = {}, aggregatedPath = "") {
+  return new Proxy(actionCallback, {
+    get(target, objKey) {
+      if (objKey in target || typeof objKey === "symbol") {
+        return target[objKey];
+      }
+      const path = aggregatedPath + encodeURIComponent(objKey.toString()).replaceAll(".", ENCODED_DOT);
+      function action(param) {
+        return handleAction(param, path, this);
+      }
+      Object.assign(action, {
+        queryString: getActionQueryString(path),
+        toString: () => action.queryString,
+        // Progressive enhancement info for React.
+        $$FORM_ACTION: function() {
+          const searchParams = new URLSearchParams(action.toString());
+          return {
+            method: "POST",
+            // `name` creates a hidden input.
+            // It's unused by Astro, but we can't turn this off.
+            // At least use a name that won't conflict with a user's formData.
+            name: "_astroAction",
+            action: "?" + searchParams.toString()
+          };
+        },
+        // Note: `orThrow` does not have progressive enhancement info.
+        // If you want to throw exceptions,
+        //  you must handle those exceptions with client JS.
+        async orThrow(param) {
+          const { data, error } = await handleAction(param, path, this);
+          if (error) throw error;
+          return data;
+        }
+      });
+      return toActionProxy(action, path + ".");
+    }
+  });
+}
+function getActionPath(action) {
+  let path = `${"/".replace(/\/$/, "")}/_actions/${new URLSearchParams(action.toString()).get(ACTION_QUERY_PARAMS.actionName)}`;
+  {
+    path = appendForwardSlash(path);
+  }
+  return path;
+}
+async function handleAction(param, path, context) {
+  if (context) {
+    const pipeline = Reflect.get(context, apiContextRoutesSymbol);
+    if (!pipeline) {
+      throw astroCalledServerError();
+    }
+    const action = await pipeline.getAction(path);
+    if (!action) throw new Error(`Action not found: ${path}`);
+    return action.bind(context)(param);
+  }
+  const headers = new Headers();
+  headers.set("Accept", "application/json");
+  let body = param;
+  if (!(body instanceof FormData)) {
+    try {
+      body = JSON.stringify(param);
+    } catch (e) {
+      throw new ActionError({
+        code: "BAD_REQUEST",
+        message: `Failed to serialize request body to JSON. Full error: ${e.message}`
+      });
+    }
+    if (body) {
+      headers.set("Content-Type", "application/json");
+    } else {
+      headers.set("Content-Length", "0");
+    }
+  }
+  const rawResult = await fetch(
+    getActionPath({
+      toString() {
+        return getActionQueryString(path);
+      }
+    }),
+    {
+      method: "POST",
+      body,
+      headers
+    }
+  );
+  if (rawResult.status === 204) {
+    return deserializeActionResult({ type: "empty", status: 204 });
+  }
+  return deserializeActionResult({
+    type: rawResult.ok ? "data" : "error",
+    body: await rawResult.text()
+  });
+}
+toActionProxy();
+
+const schema = {"RESEND_API_KEY":{"context":"server","access":"secret","type":"string"}};
+
+function getEnvFieldType(options) {
+  const optional = options.optional ? options.default !== void 0 ? false : true : false;
+  let type;
+  if (options.type === "enum") {
+    type = options.values.map((v) => `'${v}'`).join(" | ");
+  } else {
+    type = options.type;
+  }
+  return `${type}${optional ? " | undefined" : ""}`;
+}
+const stringValidator = ({ max, min, length, url, includes, startsWith, endsWith }) => (input) => {
+  if (typeof input !== "string") {
+    return {
+      ok: false,
+      errors: ["type"]
+    };
+  }
+  const errors = [];
+  if (max !== void 0 && !(input.length <= max)) {
+    errors.push("max");
+  }
+  if (min !== void 0 && !(input.length >= min)) {
+    errors.push("min");
+  }
+  if (length !== void 0 && !(input.length === length)) {
+    errors.push("length");
+  }
+  if (url !== void 0 && !URL.canParse(input)) {
+    errors.push("url");
+  }
+  if (includes !== void 0 && !input.includes(includes)) {
+    errors.push("includes");
+  }
+  if (startsWith !== void 0 && !input.startsWith(startsWith)) {
+    errors.push("startsWith");
+  }
+  if (endsWith !== void 0 && !input.endsWith(endsWith)) {
+    errors.push("endsWith");
+  }
+  if (errors.length > 0) {
+    return {
+      ok: false,
+      errors
+    };
+  }
+  return {
+    ok: true,
+    value: input
+  };
+};
+const numberValidator = ({ gt, min, lt, max, int }) => (input) => {
+  const num = parseFloat(input ?? "");
+  if (isNaN(num)) {
+    return {
+      ok: false,
+      errors: ["type"]
+    };
+  }
+  const errors = [];
+  if (gt !== void 0 && !(num > gt)) {
+    errors.push("gt");
+  }
+  if (min !== void 0 && !(num >= min)) {
+    errors.push("min");
+  }
+  if (lt !== void 0 && !(num < lt)) {
+    errors.push("lt");
+  }
+  if (max !== void 0 && !(num <= max)) {
+    errors.push("max");
+  }
+  if (int !== void 0) {
+    const isInt = Number.isInteger(num);
+    if (!(int ? isInt : !isInt)) {
+      errors.push("int");
+    }
+  }
+  if (errors.length > 0) {
+    return {
+      ok: false,
+      errors
+    };
+  }
+  return {
+    ok: true,
+    value: num
+  };
+};
+const booleanValidator = (input) => {
+  const bool = input === "true" ? true : input === "false" ? false : void 0;
+  if (typeof bool !== "boolean") {
+    return {
+      ok: false,
+      errors: ["type"]
+    };
+  }
+  return {
+    ok: true,
+    value: bool
+  };
+};
+const enumValidator = ({ values }) => (input) => {
+  if (!(typeof input === "string" ? values.includes(input) : false)) {
+    return {
+      ok: false,
+      errors: ["type"]
+    };
+  }
+  return {
+    ok: true,
+    value: input
+  };
+};
+function selectValidator(options) {
+  switch (options.type) {
+    case "string":
+      return stringValidator(options);
+    case "number":
+      return numberValidator(options);
+    case "boolean":
+      return booleanValidator;
+    case "enum":
+      return enumValidator(options);
+  }
+}
+function validateEnvVariable(value, options) {
+  const isOptional = options.optional || options.default !== void 0;
+  if (isOptional && value === void 0) {
+    return {
+      ok: true,
+      value: options.default
+    };
+  }
+  if (!isOptional && value === void 0) {
+    return {
+      ok: false,
+      errors: ["missing"]
+    };
+  }
+  return selectValidator(options)(value);
+}
+
+// @ts-check
+
+// @ts-expect-error
+/** @returns {string} */
+// used while generating the virtual module
+// biome-ignore lint/correctness/noUnusedFunctionParameters: `key` is used by the generated code
+// biome-ignore lint/correctness/noUnusedVariables: `key` is used by the generated code
+const getEnv = (key) => {
+	return getEnv$1(key);
+};
+
+const _internalGetSecret = (key) => {
+	const rawVariable = getEnv(key);
+	const variable = rawVariable === '' ? undefined : rawVariable;
+	const options = schema[key];
+
+	const result = validateEnvVariable(variable, options);
+	if (result.ok) {
+		return result.value;
+	}
+	const type = getEnvFieldType(options);
+	throw createInvalidVariablesError(key, type, result);
+};
+
+setOnSetGetEnv(() => {
+	RESEND_API_KEY = _internalGetSecret("RESEND_API_KEY");
+
+});
+let RESEND_API_KEY = _internalGetSecret("RESEND_API_KEY");
+
+async function sendEmail(options) {
+  const transporter = await getEmailTransporter();
+  return new Promise(async (resolve, reject) => {
+    const { to, subject, html, from } = options;
+    const message = {
+      from,
+      to,
+      subject,
+      html
+    };
+    transporter.sendMail(message, (err, info) => {
+      if (err) {
+        console.error(err);
+        reject(err);
+      } else {
+        resolve(info);
+      }
+    });
+  });
+}
+async function getEmailTransporter() {
+  if (!RESEND_API_KEY) {
+    throw new Error("RESEND_API_KEY is not set");
+  }
+  return new Promise((resolve, reject) => {
+    const transporter = createTransport({
+      host: "smtp.resend.com",
+      secure: true,
+      port: 465,
+      auth: { user: "resend", pass: RESEND_API_KEY }
+    });
+    resolve(transporter);
+  });
+}
+
+const server = {
+  sendEmail: defineAction({
+    accept: "form",
+    input: objectType({
+      name: stringType().min(2).max(100),
+      lastName: stringType().min(2).max(100),
+      email: stringType().email(),
+      message: stringType().min(10).max(500)
+    }),
+    handler: async (input, context) => {
+      console.log("Received input:", input);
+      try {
+        const html = `<p>Name: ${input.name}</p><p>Last Name: ${input.lastName}</p><p>Email: ${input.email}</p><p>Message: ${input.message}</p>`;
+        await sendEmail({
+          to: "cazcodavid@gmail.com",
+          subject: "New message from your website",
+          from: input.email,
+          html
+        });
+        console.log("Email sent successfully!");
+      } catch (error) {
+        console.error("Error while sending email:", error);
+      }
+    }
+  })
+};
+
+export { server };
---
+++ .vercel/output/functions/_render.func/dist/server/pages/_image.astro.mjs
@@ -0,0 +1,2 @@
+export { a as page } from '../chunks/generic_CzjzQSPM.mjs';
+export { renderers } from '../renderers.mjs';
