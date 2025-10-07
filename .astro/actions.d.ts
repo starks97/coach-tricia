624cba6d58ab75dcf91fb134412ff6ca75cfe136
David Espinoza <81840518+starks97@users.noreply.github.com>
2025-10-07 17:33:00 -0400

    Develop (#3)
    * feature: nice
    
    * new button for form

--- .astro/actions.d.ts
+++
@@ -1,5 +0,0 @@
-declare module "astro:actions" {
-	type Actions = typeof import("/Users/jeffersonespinoza/Desktop/projects/coach-tricia/src/actions/index.ts")["server"];
-
-	export const actions: Actions;
-}
